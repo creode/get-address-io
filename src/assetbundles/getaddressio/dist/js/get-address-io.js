7c2aeb898fe26c3ad38ede9b4630cfcf7a716196
Jamie Sykes <jamie.sykes@creode.co.uk>
2021-08-31 11:46:10 +0100

    Update the event to use jQuery to keep things consistent.

--- src/assetbundles/getaddressio/dist/js/get-address-io.js
+++ src/assetbundles/getaddressio/dist/js/get-address-io.js
@@ -87,7 +87,7 @@ var getAddressLookup = {
 
     focusSelectAutocompleteOnOpen: function() {
         jQuery(document).on('select2:open', () => {
-            document.querySelector('.select2-container--open .select2-search__field').focus();
+            jQuery('.select2-container--open .select2-search__field').focus();
         });
     },
 
