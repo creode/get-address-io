a3a3849ff0c507fa9052643baed2d3f7423ffa35
Jamie Sykes <jamie.sykes@creode.co.uk>
2021-09-01 10:22:48 +0100

    Refactors interface to add more required functions.

--- src/contracts/services/ApiFactory.php
+++ src/contracts/services/ApiFactory.php
@@ -27,6 +27,22 @@ abstract class ApiFactory extends Component
     abstract public function autocomplete(string $searchTerm): object;
 
     /**
+     * Gets an Address from the API with the provided ID.
+     *
+     * @param string $id
+     * @return object
+     */
+    abstract public function getAddressById(string $id): object;
+
+    /**
+     * Gets all address' associated to a provided postcode.
+     *
+     * @param string $postcode
+     * @return object
+     */
+    abstract public function getAddressesByPostcode(string $postcode): object;
+
+    /**
      * Builds a predefined guzzle client with the correct options.
      *
      * @return GuzzleClient
@@ -42,12 +58,13 @@ abstract class ApiFactory extends Component
     }
 
     /**
-     * Accepts a url parameter and calls the API with the given url.
+     * Accepts a url parameter and pushes a GET request to the API with the given url.
      *
-     * @param string $url
+     * @param string $url The url to call.
+     * @param array $parameters Any query parameters provided.
      * @return object
      */
-    protected function callAddressAPI($url)
+    protected function callAddressAPI($url, $parameters = [])
     {
         if (empty(GetAddressIo::$plugin->getSettings()->getAPIKey())) {
             // Throw an error.
@@ -59,14 +76,15 @@ abstract class ApiFactory extends Component
         $response = false;
         $errors = [];
 
+        $queryParams = ['api-key' => GetAddressIo::$plugin->getSettings()->getAPIKey()];
+        $queryParams = array_merge($queryParams, $parameters);
+
         try {
             $response = $client->request(
                 'GET',
                 $url,
                 [
-                    'query' => [
-                        'api-key' => GetAddressIo::$plugin->getSettings()->getAPIKey(),
-                    ],
+                    'query' => $queryParams,
                 ],
             );
         } catch (\Exception $e) {
